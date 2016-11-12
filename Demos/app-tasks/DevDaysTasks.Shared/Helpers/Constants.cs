23bf1bd68cb9c21b176907e16e461668e35c13e5
Peter Joseph Concha Regatto <peter.concha@avanet.org>
2016-11-12 13:19:35 -0500

    Actualización de referencias y llamado al servicio.

--- Demos/app-tasks/DevDaysTasks.Shared/Helpers/Constants.cs
+++ Demos/app-tasks/DevDaysTasks.Shared/Helpers/Constants.cs
@@ -3,10 +3,15 @@
 namespace DevDaysTasks
 {
 	public static class Constants
-	{
-		// Replace strings with your mobile services and gateway URLs.
-		public static string ApplicationURL =
+    {
+        // Replace strings with your mobile services and gateway URLs.
+        /*
+         * public static string ApplicationURL =
             @"https://OUR-APP-NAME-HERE.azurewebsites.net";
+            */
+            public static string ApplicationURL =
+            @"http://pruebaxamarindevdayquito.azurewebsites.net";
+        
 	}
 }
 
